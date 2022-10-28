d49ed62d0779fa81a358931f9f08e64aa853de6c
s-tw-tools <s-tw-tools@users.noreply.github.com>
2022-10-28 15:27:03 +0000

    deploy: 10f51d31a700c1a99b9b65d82009957aa91f800c

---
+++ basicTools.js
@@ -0,0 +1,20 @@
+let TSK = (function(){
+    function TSK() {        
+        this.sleep = async function sleep(s) {
+          await new Promise(r => setTimeout(r, (Math.floor((Math.random() * 300) + s * 1000))));
+        }
+    }
+  
+    let instance;
+  
+    return {
+      getTSK: function(){
+        if (!instance) {
+          instance = new TSK();
+          delete instance.constructor;
+        }
+        return instance;
+      }
+    };
+})();
+TSK.getTSK();
