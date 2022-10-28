982f31c9223932b759acc6dffc00250a63374022
s-tw-tools <s-tw-tools@users.noreply.github.com>
2022-10-28 16:08:10 +0000

    deploy: ff62f29f398929d53d62891cddcd758cc42bdbb4

---
+++ walker.js
@@ -0,0 +1,82 @@
+let walkerCollection = (function(){
+    function Singleton() {      
+      if(Bag.getItemCount(699000) == 0)
+        return;
+      
+      this.createButton = function() {
+        var icon = $('<div></div>')
+          .attr({
+            class: 'menulink',
+            title: 'Wooosh!',
+          })
+          .css({
+            'background': 'url(https://westde.innogamescdn.com/images/icons/turtle.png)',
+            'background-position': '2px 6px',
+            'background-repeat': 'no-repeat'
+          })
+          .mouseleave(function () {
+            $(this).css('background-position', '2px 6px');
+          })
+          .mouseenter(function (e) {
+            $(this).css('background-position', '5px 9px');
+          })
+          .click(function () {
+            switchPosition();
+          });
+        var bottom = $('<div></div>').attr({
+          class: 'menucontainer_bottom',
+        });
+        $('#ui_menubar .ui_menucontainer:last').after(
+          $('<div></div>')
+            .attr({
+              class: 'ui_menucontainer',
+              id: 'TUUUUUUURTLE',
+            })
+          .append(icon)
+          .append(bottom)
+        );
+      }
+      
+      this.startWalk = async function (fortId){
+          let old = Wear.get("animal")
+          await TSK.sleep(0.01);
+          Wear.carry(Bag.getItemByItemId(699000))
+          TaskQueue.add(TaskWalk(fortId,"fort"))
+          await TSK.sleep(1.1);
+          Wear.carry(old)
+      }
+      this.switchPositionW10 = async function(){
+          if(Character.position.x > 20000)
+            startWalk(144);
+          else
+            startWalk(158);
+      }
+      this.switchPositionFairbanks = async function(){
+        if(Character.position.x > 20000)
+          startWalk(16);
+        else
+          startWalk(8);
+      }
+      
+      this.switchPosition = async function() {
+        if(Game.worldName == "Welt 10")
+          switchPositionW10();
+        if(Game.worldName == "Fairbanks")
+          switchPositionFairbanks();
+      }
+    }
+  
+    let instance;
+  
+    return {
+      init: function(){
+        if (!instance) {
+          instance = new Singleton();
+          instance.createButton();
+          delete instance.constructor;
+        }
+        return instance;
+      }
+    };
+})();
+walkerCollection.init();
