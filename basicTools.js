493aba87eb19a762704b2a37d30263c8dd6d44af
s-tw-tools <s-tw-tools@users.noreply.github.com>
2022-10-28 16:37:54 +0000

    deploy: 67533bbcd611d883c53ff39122ed880291008f8e

--- basicTools.js
+++ basicTools.js
@@ -3,6 +3,17 @@ let functionCollection = (function(){
         this.sleep = async function sleep(s) {
           await new Promise(r => setTimeout(r, (Math.floor((Math.random() * 300) + s * 1000))));
         }
+        
+        this.showMessage = async function (str){
+            let a = "";
+            a = new west.gui.Dialog(str)
+            a.setY(100)
+            a.setX(500);
+            a.show();
+            a.setBlockGame(false);
+            await sleep(1.5);
+            a.hide();
+        }
     }
   
     let instance;
