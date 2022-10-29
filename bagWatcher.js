270aca9647f5d77b6b399bffeb3000ce4b7ab49a
s-tw-tools <116835989+s-tw-tools@users.noreply.github.com>
2022-10-29 22:54:00 +0200

    Update bagWatcher.js

--- bagWatcher.js
+++ bagWatcher.js
@@ -1,20 +1,21 @@
 (async function() {
     async function downTimeChecker(item_id){
-        await TSK.sleep(Math.random() * 10 + 5)
+        await TSK.sleep(Math.random() * 10 + 60)
         do
             await TSK.sleep(10 + Math.random() * 5)
         while(new Date(Bag.getItemByItemId(item_id).cooldown * 1000) > new Date())
             await TSK.sleep(Math.random() * 1000)
         ItemUse.doIt(item_id)
     }
-
-    downTimeChecker(50691000)
-    downTimeChecker(2557000)
-    downTimeChecker(2558000)
-    downTimeChecker(2698000)
-    downTimeChecker(2482000)
-    downTimeChecker(51482000)
-    downTimeChecker(51483000)
-    downTimeChecker(2665000)
-    downTimeChecker(2666000)
+    try {
+        downTimeChecker(50691000)
+        downTimeChecker(2557000)
+        downTimeChecker(2558000)
+        downTimeChecker(2698000)
+        downTimeChecker(2482000)
+        downTimeChecker(51482000)
+        downTimeChecker(51483000)
+        downTimeChecker(2665000)
+        downTimeChecker(2666000)
+    } catch(err){}
 })();
